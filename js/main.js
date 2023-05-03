331157a4288231145c25a0d8805f8478c6a047a9
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-03 14:14:07 +0200

    comment ex mail

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// creare un array con una lista di nomi
+
+// creare un cuiclo con i moni
+    // se il nome dell'utente è presente allora gli chiedo l'email
