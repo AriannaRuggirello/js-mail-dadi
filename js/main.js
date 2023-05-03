52ae231900fdebd3c70abe3695751e064eb87431
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-03 14:54:25 +0200

    mail js and html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-// creare un array con una lista di nomi
+// creare un array con una lista di email
 
-// creare un cuiclo con i moni
-    // se il nome dell'utente è presente allora gli chiedo l'email
+const userEmails = ['lucarossi@gmail.com', 'nomecognome@gmail.com', 'pincopallo@gmail.com', 'rosabianchi@gmail.com'];
+
+// creare una funzione che chieda all'utente di inserire la sua email
+function emailVerification() {
+
+    // email dell'utente
+    let email = document.getElementById("email").value;
+    console.log(email);
+
+    // creare un ciclo con le email
+    for(let i = 0; i < userEmails.length; i++){
+        if(userEmails.includes(email)) {
+            // se il nome dell'utente è presente allora gli chiedo l'email
+            console.log("benvenuto pinco pallo!");
+        }
+
+            else{
+                console.log("non sei ancora registrato,registrati!");
+            }
+    }
+
+}
+    
