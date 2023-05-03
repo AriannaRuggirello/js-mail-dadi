74b5bb50fa0aa4c9795f1a23f1fbe2f795a807db
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-03 15:12:32 +0200

    mail js update

--- js/main.js
+++ js/main.js
@@ -2,26 +2,29 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
 // creare un array con una lista di email
-
-const userEmails = ['lucarossi@gmail.com', 'nomecognome@gmail.com', 'pincopallo@gmail.com', 'rosabianchi@gmail.com'];
+const email = ['lucarossi@gmail.com', 'nomecognome@gmail.com', 'pincopallo@gmail.com', 'rosabianchi@gmail.com'];
+console.log(email);
 
 // creare una funzione che chieda all'utente di inserire la sua email
 function emailVerification() {
 
     // email dell'utente
-    let email = document.getElementById("email").value;
-    console.log(email);
+    let userEmail = document.getElementById("email").value;
+
+    console.log(userEmail);
 
     // creare un ciclo con le email
     for(let i = 0; i < userEmails.length; i++){
+        
+        // se il nome dell'utente è presente allora gli chiedo l'email
         if(userEmails.includes(email)) {
-            // se il nome dell'utente è presente allora gli chiedo l'email
-            console.log("benvenuto pinco pallo!");
+            
+            console.log("benvenuto!");
         }
-
-            else{
-                console.log("non sei ancora registrato,registrati!");
-            }
+        // altrimenti nega accesso’
+        else{
+            console.log("non sei ancora registrato,registrati!");
+        }
     }
 
 }
