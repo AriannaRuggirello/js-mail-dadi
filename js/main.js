e0fd7402e264d1a24ce54bce2c7a43cbe179a706
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-03 15:40:02 +0200

    gioco dei dadi js comment

--- js/main.js
+++ js/main.js
@@ -24,11 +24,20 @@ function emailVerification() {
         document.getElementById("output").innerHTML = "Non sei ancora registrato. Registrati!";
         // console.log("non sei ancora registrato,registrati!");
     }
-
-    
-
-        
-   
-
 }
-    
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+// Generare un numero random da 1 a 6 per il computer
+
+// Generare un numero random da 1 a 6 per il giocatore 
+
+// (if) il giocatore ha un numero maggiore del computer vince il giocatore  
+
+// (else if) il computer ha il numero più alto del giocatore vince il computer
+
+// (else) se hanno il numero uguale, pareggiano
